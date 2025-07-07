dc56b86fee1096b4bbf8d517aa5d3cbac7d4c371
Francisco <github.backstage538@passmail.net>
2025-07-07 21:04:28 +0200

    Load events

--- src/features/events/dashboard/event-dashboard.vue
+++ src/features/events/dashboard/event-dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex w-full flex-row gap-6">
     <div class="flex w-3/5 flex-col gap-4">
-      <EventCard v-for="event of events" :key="event.id" :event />
+      <EventCard v-for="event of appEvents" :key="event.id" :event />
     </div>
     <div class="w-2/5">
       <EventForm v-if="isFormOpen" @cancel="$emit('close')" />
@@ -13,7 +13,19 @@
 import EventCard from '@/features/events/dashboard/event-card.vue';
 import EventForm from '@/features/events/form/event-form.vue';
 import { events } from '@/lib/data/sample-data';
+import type { AppEvent } from '@/lib/types';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+const appEvents = ref<AppEvent[]>([]);
 
 defineProps<{ isFormOpen: boolean }>();
 defineEmits<{ close: void }>();
+
+onMounted(() => {
+  appEvents.value = events;
+});
+
+onBeforeUnmount(() => {
+  appEvents.value = [];
+});
 </script>
