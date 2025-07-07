2c28b7c0a8f76bcf17dbf70b76c31fd9504d2c39
Francisco <github.backstage538@passmail.net>
2025-07-07 18:06:42 +0200

    Create event form

--- src/features/events/dashboard/event-dashboard.vue
+++ src/features/events/dashboard/event-dashboard.vue
@@ -3,10 +3,13 @@
     <div class="flex w-3/5 flex-col gap-4">
       <EventCard v-for="i of 10" :key="i" />
     </div>
-    <div class="w-2/5">Right content</div>
+    <div class="w-2/5">
+      <EventForm />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import EventCard from '@/features/events/dashboard/event-card.vue';
+import EventForm from '@/features/events/form/event-form.vue';
 </script>
---
+++ src/features/events/form/event-form.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card bg-base-100 flex w-full flex-col gap-3 p-4">
+    <h3 class="text-primary text-center text-2xl font-semibold">
+      Create new event
+    </h3>
+    <form class="flex w-full flex-col gap-3">
+      <input
+        type="text"
+        placeholder="Event title"
+        class="input input-lg w-full"
+      />
+      <input type="text" placeholder="Category" class="input input-lg w-full" />
+      <textarea
+        type="text"
+        placeholder="Description"
+        class="textarea textarea-lg w-full"
+      />
+      <input type="text" placeholder="Date" class="input input-lg w-full" />
+      <input type="text" placeholder="City" class="input input-lg w-full" />
+      <input type="text" placeholder="Venue" class="input input-lg w-full" />
+      <div class="flex w-full justify-end gap-3">
+        <button class="btn btn-neutral">Cancel</button>
+        <button class="btn btn-primary">Create</button>
+      </div>
+    </form>
+  </div>
+</template>
