644b7ca6f621661478dd3a58ff882093ad74bf16
Francisco <github.backstage538@passmail.net>
2025-07-08 22:21:29 +0200

    WIP

--- src/features/events/dashboard/event-dashboard.vue
+++ src/features/events/dashboard/event-dashboard.vue
@@ -39,14 +39,14 @@ import { onBeforeUnmount, onMounted, ref } from 'vue';
 const appEvents = ref<AppEvent[]>([]);
 
 defineProps<{ isFormOpen: boolean }>();
-const emit = defineEmits<{ close: [void] }>();
+defineEmits<{ close: [void] }>();
 
-function handleCreateEvent(eventData: AppEvent): void {
-  appEvents.value.push(eventData);
-  emit('close');
-}
+// function handleCreateEvent(eventData: AppEvent): void {
+//   appEvents.value.push(eventData);
+//   emit('close');
+// }
 
-function handleUpdateEvent(updatedEventData: AppEvent): void {}
+// function handleUpdateEvent(updatedEventData: AppEvent): void {}
 
 onMounted(() => {
   appEvents.value = events;
